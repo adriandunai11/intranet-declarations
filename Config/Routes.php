1ec8c3eb5ad6aada2b6b8f274911ac587e2602db
adriandunai11 <dunaiadrian1113@gmail.com>
2026-07-03 12:56:09 +0200

    Add candidate optional declaration removal route

--- Config/Routes.php
+++ Config/Routes.php
@@ -41,6 +41,7 @@ $routes->group('', [
     $routes->get('start/(:segment)/item/(:num)', 'InvitationController::item/$1/$2');
     $routes->get('start/(:segment)/item/(:num)/preview', 'DocumentPreviewController::item/$1/$2');
     $routes->post('start/(:segment)/tax-template/(:num)/select', 'InvitationController::selectTaxTemplate/$1/$2');
+    $routes->post('start/(:segment)/item/(:num)/remove', 'InvitationController::removeItem/$1/$2');
     $routes->post('start/(:segment)/finalize', 'InvitationController::finalize/$1');
     $routes->post('start/(:segment)/item/(:num)', 'InvitationController::submitItem/$1/$2');
 });
